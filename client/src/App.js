01d83b063c553ad4cff5f6752eca86fca91527e7
Tin <tin.doann@gmail.com>
2021-07-09 19:12:28 -0400

    Build: dynamic geoloc and variables

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
 import * as React from "react";
-import { useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+import "./app.css";
+import axios from "axios";
 
 function App() {
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [viewport, setViewport] = useState({
-    width: "100vh",
+    width: "150 vh",
     height: "100vh",
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8,
   });
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
+
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id);
+  };
 
   return (
     <div className="App">
@@ -17,15 +37,53 @@ function App() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        mapStyle="mapbox://styles/tindoku/ckqwup6th0sc017uogp9upi89"
       >
-        <Marker
-          latitude={37.78}
-          longitude={-122.41}
-          offsetLeft={-20}
-          offsetTop={-10}
-        >
-          <div>You are here</div>
-        </Marker>
+        {pins.map((p) => (
+          <>
+            <Marker
+              latitude={p.lat}
+              longitude={p.long}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <Room
+                style={{
+                  fontSize: viewport.zoom * 6,
+                  color: p.username === currentUser ? "tomato" : "slateblue",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleMarkerClick(p.id)}
+              />
+            </Marker>
+            {p.id === currentPlaceId && (
+              <Popup
+                latitude={37.78}
+                longitude={-122.41}
+                closeButton={true}
+                closeOnClick={false}
+                anchor="left"
+                onClose={() => setCurrentPlaceId(null)}
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    <Star />
+                  </div>
+                  <label>Info</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  {/* <span className="date">{format(p.createdAt)}</span> */}
+                </div>
+              </Popup>
+            )}
+          </>
+        ))}
       </ReactMapGL>
     </div>
   );
