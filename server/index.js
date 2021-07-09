a0e9ee8f832f41e2cf8f6f48a823438d884ef6a0
Tin <tin.doann@gmail.com>
2021-07-09 04:15:15 -0400

    Build: connect to mongodb server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Mongo Connected");
+  })
+  .catch((err) => console.log(err));
 
 app.listen(4000, () => {
   console.log("Server is running!");
