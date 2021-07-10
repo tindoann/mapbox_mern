588e241a7f3a35cab229b68dc0d1b9cd1be19fa2
Tin <tin.doann@gmail.com>
2021-07-09 21:11:17 -0400

    Build: dynamic popover input

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,14 @@ import "./app.css";
 import axios from "axios";
 
 function App() {
+  const currentUser = "paul";
   const [pins, setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [rating, setRating] = useState(null);
+  const [star, setStar] = useState(0);
   const [viewport, setViewport] = useState({
     width: "150 vh",
     height: "100vh",
@@ -27,10 +33,37 @@ function App() {
     getPins();
   }, []);
 
-  const handleMarkerClick = (id) => {
+  const handleMarkerClick = (id, lat, long) => {
     setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude: lat, longitude: long });
   };
 
+  const handleAddClick = (e) => {
+    const [long, lat] = e.lngLat;
+    setNewPlace({
+      lat,
+      long,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+    try {
+      const res = await axios.post("/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="App">
       <ReactMapGL
@@ -38,14 +71,16 @@ function App() {
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
         mapStyle="mapbox://styles/tindoku/ckqwup6th0sc017uogp9upi89"
+        onDblClick={handleAddClick}
+        transitionDuration="200"
       >
         {pins.map((p) => (
           <>
             <Marker
               latitude={p.lat}
               longitude={p.long}
-              offsetLeft={-20}
-              offsetTop={-10}
+              offsetLeft={-viewport.zoom * 3.5}
+              offsetTop={-viewport.zoom * 7}
             >
               <Room
                 style={{
@@ -53,7 +88,7 @@ function App() {
                   color: p.username === currentUser ? "tomato" : "slateblue",
                   cursor: "pointer",
                 }}
-                onClick={() => handleMarkerClick(p.id)}
+                onClick={() => handleMarkerClick(p.id, p.lat, p.long)}
               />
             </Marker>
             {p.id === currentPlaceId && (
@@ -72,7 +107,7 @@ function App() {
                   <p className="desc">{p.desc}</p>
                   <label>Rating</label>
                   <div className="stars">
-                    <Star />
+                    {Array(p.rating).fill(<Star className="star" />)}
                   </div>
                   <label>Info</label>
                   <span className="username">
@@ -84,6 +119,40 @@ function App() {
             )}
           </>
         ))}
+        {newPlace && (
+          <Popup
+            latitude={newPlace.lat}
+            longitude={newPlace.long}
+            closeButton={true}
+            closeOnClick={false}
+            anchor="left"
+            onClose={() => setNewPlace(null)}
+          >
+            <div>
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input
+                  placeholder="Enter a title"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Review</label>
+                <textarea
+                  placeholder="Tell us something about this place"
+                  onChange={(e) => setDesc(e.target.value)}
+                />
+                <label>Rating</label>
+                <select onChange={(e) => setRating(e.target.value)}>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button className="submitButton">Add Pin</button>
+              </form>
+            </div>
+          </Popup>
+        )}
       </ReactMapGL>
     </div>
   );
